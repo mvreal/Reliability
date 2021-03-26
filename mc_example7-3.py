a9b2ea6c5a94bc0a9b3c2c07a608d1da74709200
Mauro Real <mvrealgm@gmail.com>
2021-03-26 18:06:15 -0300

    Example 7-3 reviewed

--- mc_example7-3.py
+++ mc_example7-3.py
@@ -4,9 +4,7 @@ Reliabilty Analysis
 Example 7.3 - Linear limit state function with normal independent variables
 @author: MVREAL
 """
-import numpy as np
 from class_reliability import *
-import time
 
 #
 # Step 0 - Column: g(R, G, Q, W) = R-G-Q-W = 0
@@ -19,7 +17,6 @@ def gfunction(x):
     return g
 
 
-ti = time.time()
 #
 # Data input
 #
@@ -36,8 +33,5 @@ xvar = [
 # MCS method
 #
 column = Reliability(xvar, gfunction)
-column.mc(20, 5000, 0.01, 1.00)
-tf = time.time()
-ttotal = tf - ti
-print(f'Processing time = {ttotal}')
+column.mc(20, 5000, 0.03, 1.00)
 #
