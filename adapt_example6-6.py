2f69fc92993dc6093d0879376bd74082b886d183
Mauro Real <mvrealgm@gmail.com>
2021-09-17 20:25:38 -0300

    Example 6-6

---
+++ adapt_example6-6.py
@@ -0,0 +1,44 @@
+"""
+Created on Mon Sep 17 17:00:00 2021
+Reliabilty Analysis
+Example 6.6 - Nonlinear limit state function with non-normal variables
+@author: MVREAL
+"""
+from realpy import *
+
+#
+# Step 0 - Beam: g(Y, Z, M) = Y*Z-M = 0
+#
+
+
+def gfunction(x, d):
+
+    g = d[0]*x[0]*x[1]-d[1]*x[2]
+    return g
+
+
+#
+# Data input
+#
+# Random variables: name, probability distribution, mean and coefficient of variation
+
+xvar = [
+    {'varname': 'Y', 'vardist': 'lognormal', 'varmean': 38.00, 'varcov': 0.10},
+    {'varname': 'Z', 'vardist': 'normal', 'varmean': 60.00, 'varcov': 0.05},
+    {'varname': 'M', 'vardist': 'frechet', 'varmean': 1000.00, 'varcov': 0.30}
+]
+# Design variables
+
+dvar = [
+    {'varname': 'gamma1', 'varvalue': 1.00},
+    {'varname': 'gamma2', 'varvalue': 1.00}
+]
+
+# Correlation matrix
+
+#
+# MC-IS adaptative method
+#
+beam = Reliability(xvar, dvar, gfunction, None, None)
+beam.adaptive(100, 5000, 0.05, 1.50)
+#
